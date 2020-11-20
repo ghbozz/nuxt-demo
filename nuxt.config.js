43bafeec96ff685746a11ccc9300438d5110c206
romain sanson <romainsanson.gm@gmail.com>
2020-11-20 01:01:10 +0100

    cneaup

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,10 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+
+  router: {
+    base: '/nuxt-demo/'
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
